7f75123a44b85d13d857563c9171ee81f1ccd8ea
MaxwellJoneZ <axel.pfister@gmail.com>
2026-09-23 10:45:41 +0200

    Add creative tab entries for drying rack
    Added a listener to include the drying rack in the functional blocks creative tab.

--- src/main/java/io/github/maxwelljones/tierworks/Tierworks.java
+++ src/main/java/io/github/maxwelljones/tierworks/Tierworks.java
@@ -3,9 +3,11 @@ package io.github.maxwelljones.tierworks;
 import com.mojang.logging.LogUtils;
 import io.github.maxwelljones.tierworks.registry.ModBlocks;
 import io.github.maxwelljones.tierworks.registry.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import org.slf4j.Logger;
 
 @Mod(Tierworks.MOD_ID)
@@ -16,6 +18,14 @@ public final class Tierworks {
     public Tierworks(IEventBus modEventBus, ModContainer modContainer) {
         ModBlocks.register(modEventBus);
         ModItems.register(modEventBus);
+        modEventBus.addListener(this::addCreativeTabEntries);
+
         LOGGER.info("Tierworks initialized");
+    }
+
+    private void addCreativeTabEntries(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
+            event.accept(ModItems.DRYING_RACK.get());
+        }
     }
 }

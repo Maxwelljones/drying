d1d1107873e668027d11d0c538ae7c75db0bc32b
MaxwellJoneZ <axel.pfister@gmail.com>
2026-09-23 10:50:44 +0200

    Add DeferredItem for Drying Rack block item

--- src/main/java/io/github/maxwelljones/tierworks/registry/ModItems.java
+++ src/main/java/io/github/maxwelljones/tierworks/registry/ModItems.java
@@ -1,11 +1,16 @@
 package io.github.maxwelljones.tierworks.registry;
 
 import io.github.maxwelljones.tierworks.Tierworks;
+import net.minecraft.world.item.BlockItem;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public final class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Tierworks.MOD_ID);
+
+    public static final DeferredItem<BlockItem> DRYING_RACK =
+            ITEMS.registerSimpleBlockItem("drying_rack", ModBlocks.DRYING_RACK);
 
     private ModItems() {
     }
